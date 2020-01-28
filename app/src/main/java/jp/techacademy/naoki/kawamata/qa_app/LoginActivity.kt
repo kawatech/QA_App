a61ab7749483c35ce584c52b796976f322d11c40
kawatech <kawatech@gol.com>
2020-01-28 20:41:06 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/naoki/kawamata/qa_app/LoginActivity.kt
@@ -0,0 +1,12 @@
+package jp.techacademy.naoki.kawamata.qa_app
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
